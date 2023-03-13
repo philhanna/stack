35c3f90c66a3cdf5948ba41dec9d109edcc8f890
Phil Hanna <ph1204@gmail.com>
2023-03-13 11:20:51 -0400

    Made generic

--- stack.go
+++ stack.go
@@ -4,16 +4,16 @@ package stack
 // Type definitions
 // ---------------------------------------------------------------------
 
-type Stack struct {
-	list []string
+type Stack[T any] struct {
+	list []T
 }
 
 // ---------------------------------------------------------------------
 // Constructor
 // ---------------------------------------------------------------------
 
-func NewStack() *Stack {
-	p := new(Stack)
+func NewStack[T any]() *Stack[T] {
+	p := new(Stack[T])
 	return p
 }
 
@@ -21,27 +21,27 @@ func NewStack() *Stack {
 // Methods
 // ---------------------------------------------------------------------
 
-func (p *Stack) Len() int {
+func (p *Stack[T]) Len() int {
 	return len(p.list)
 }
 
-func (p *Stack) Push(s string) {
-	p.list = append(p.list, s)
+func (p *Stack[T]) Push(item T) {
+	p.list = append(p.list, item)
 }
 
-func (p *Stack) Pop() string {
+func (p *Stack[T]) Pop() T {
 	if p.Len() > 0 {
 		result := p.list[p.Len()-1]
 		p.list = p.list[:p.Len()-1]
 		return result
 	}
-	return ""
+	return *(new(T))
 }
 
-func (p *Stack) Peek() string {
+func (p *Stack[T]) Peek() T {
 	if p.Len() > 0 {
 		result := p.list[p.Len()-1]
 		return result
 	}
-	return ""
+	return *(new(T))
 }
--- stack_test.go
+++ stack_test.go
@@ -3,18 +3,43 @@ package stack
 import "testing"
 import "github.com/stretchr/testify/assert"
 
-func TestStack_PushPop(t *testing.T) {
-	stack := NewStack()
+func TestStack_String(t *testing.T) {
+	var stooge string
+
+	stack := NewStack[string]()
 	stack.Push("Larry")
 	stack.Push("Curly")
 	stack.Push("Moe")
 
-	stooge := stack.Pop()
+	assert.Equal(t, 3, stack.Len())
+	assert.Equal(t, "Moe", stack.Peek())
+	stooge = stack.Pop()
 	assert.Equal(t, "Moe", stooge)
 	stooge = stack.Pop()
 	assert.Equal(t, "Curly", stooge)
 	stooge = stack.Pop()
 	assert.Equal(t, "Larry", stooge)
+	stooge = stack.Pop()
+	assert.Equal(t, "", stooge)
+}
 
-	assert.Equal(t, 0, stack.Len())
+func TestStack_Int(t *testing.T) {
+
+	var digit int
+
+	stack := NewStack[int]()
+	stack.Push(3)
+	stack.Push(1)
+	stack.Push(4)
+	
+	assert.Equal(t, 3, stack.Len())
+	assert.Equal(t, 4, stack.Peek())
+	digit = stack.Pop()
+	assert.Equal(t, 4, digit)
+	digit = stack.Pop()
+	assert.Equal(t, 1, digit)
+	digit = stack.Pop()
+	assert.Equal(t, 3, digit)
+	digit = stack.Pop()
+	assert.Equal(t, 0, digit)
 }
