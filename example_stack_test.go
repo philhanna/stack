cd5e8a2736ce168e809da411e866b2b58f641573
Phil Hanna <ph1204@gmail.com>
2023-03-14 09:44:37 -0400

    Added example

---
+++ example_stack_test.go
@@ -0,0 +1,40 @@
+package stack
+
+import (
+	"fmt"
+)
+
+// ExampleStack illustrates how to use the Stack type.
+func ExampleStack() {
+
+	var item string
+
+	// Create a stack for strings
+	sst := NewStack[string]()
+
+	// Push three strings on the stack
+	sst.Push("A")
+	sst.Push("B")
+	sst.Push("C")
+
+	// Get the stack length
+	length := sst.Len()
+	fmt.Println(length)
+
+	// Peek returns the top element without altering the stack
+	item, _ = sst.Peek()
+	fmt.Printf("%s,%d\n", item, sst.Len())
+
+	// Pop returns the top element and removes it from the stack
+	for !sst.IsEmpty() {
+		item, _ = sst.Pop()
+		fmt.Println(item)
+	}
+
+	// Output:
+	// 3
+	// C,3
+	// C
+	// B
+	// A
+}
