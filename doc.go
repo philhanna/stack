1f64096f6a3e1b863c8fd4f2ed11979ca741070a
Phil Hanna <ph1204@gmail.com>
2023-03-14 10:03:35 -0400

    Updated documentation

--- doc.go
+++ doc.go
@@ -1,15 +1,14 @@
 /*
 Stack is a library for using LIFO stacks of objects of any type.
-It has all the familiar operations:
+It has the familiar operations:
 
-	Len(), which returns the number of items in the stack
-	IsEmpty(), which returns true if the stack has no items
-	Push(item any), which adds an item to the stack
-	Pop(), which removes and returns an element from the stack
-	Peek(), which returns the element on the top of the stack without removing it
+	Len(), which returns the number of elements in the stack.
+	IsEmpty(), which returns true if the stack has no elements.
+	Push(), which adds an element to the stack.
+	Pop(), which removes and returns an element from the stack,
+	Peek(), which returns the element on the top of the stack without removing it.
+	Clear(), which removes all elements from the stack.
 
-Pop() and Peek() do not fail when called on an empty stack, they just
-return the zero value of the stack item type. This is for convenience
-in not returning two values.
+Pop() and Peek() fail when called on an empty stack.
 */
 package stack
