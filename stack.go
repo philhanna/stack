a68aae233e1fd3d914386cdb8c13d0a363530cd4
Phil Hanna <ph1204@gmail.com>
2023-03-14 09:22:23 -0400

    Made stack generic but items of same type.
    Fix for issue #1

--- stack.go
+++ stack.go
@@ -1,10 +1,12 @@
 package stack
 
+import "errors"
+
 // ---------------------------------------------------------------------
 // Type definitions
 // ---------------------------------------------------------------------
 
-// Stack is a LIFO list for elements of type T
+// Stack is a LIFO list of items.
 type Stack[T any] struct {
 	list []T
 }
@@ -13,49 +15,58 @@ type Stack[T any] struct {
 // Constructor
 // ---------------------------------------------------------------------
 
-// NewStack creates a new stack of type T and returns a pointer to it.
-func NewStack[T any]() *Stack[T] {
-	p := new(Stack[T])
-	return p
+// NewStack creates a new stack and returns it.
+func NewStack[T any]() Stack[T] {
+	pst := new(Stack[T])
+	return *pst
 }
+
+// ---------------------------------------------------------------------
+// Error messages
+// ---------------------------------------------------------------------
+
+var ErrorStackEmpty = errors.New("Stack empty")
 
 // ---------------------------------------------------------------------
 // Methods
 // ---------------------------------------------------------------------
 
 // Len returns the number of items in the stack.
-func (p *Stack[T]) Len() int {
-	return len(p.list)
+func (pst *Stack[T]) Len() int {
+	return len(pst.list)
 }
 
 // IsEmpty returns true if the stack depth is zero.
-func (p *Stack[T]) IsEmpty() bool {
-	return p.Len() == 0
+func (pst *Stack[T]) IsEmpty() bool {
+	return pst.Len() == 0
 }
 
-// Push appends an item of type T to the stack.
-func (p *Stack[T]) Push(item T) {
-	p.list = append(p.list, item)
+// Push appends an item onto the stack.
+func (pst *Stack[T]) Push(item T) {
+	pst.list = append(pst.list, item)
 }
 
-// Pop removes the topmost item from the stack and returns it.  If the
-// stack is empty, returns the zero form of type T.
-func (p *Stack[T]) Pop() T {
-	if p.Len() > 0 {
-		result := p.list[p.Len()-1]
-		p.list = p.list[:p.Len()-1]
-		return result
+// Pop removes the topmost item from the stack and returns it. If the
+// stack is empty, returns an error
+func (pst *Stack[T]) Pop() (T, error) {
+	empty := new(T)
+	if pst.IsEmpty() {
+		return *empty, ErrorStackEmpty
 	}
-	return *(new(T))
+	last := pst.Len() - 1
+	result := pst.list[last]
+	pst.list = pst.list[:last]
+	return result, nil
 }
 
-// Peek returns the topmost item from the stack and returns it.  The
-// stack is not modified.  If the stack is empty, returns the zero form
-// of type T.
-func (p *Stack[T]) Peek() T {
-	if p.Len() > 0 {
-		result := p.list[p.Len()-1]
-		return result
+// Peek returns the topmost item from the stack and returns it. The
+// stack is not modified. If the stack is empty, returns an error.
+func (pst *Stack[T]) Peek() (T, error) {
+	empty := new(T)
+	if pst.IsEmpty() {
+		return *empty, ErrorStackEmpty
 	}
-	return *(new(T))
+	last := pst.Len() - 1
+	result := pst.list[last]
+	return result, nil
 }
--- stack_test.go
+++ stack_test.go
@@ -1,158 +1,102 @@
 package stack
 
 import (
-	"encoding/xml"
-	"flag"
-	"fmt"
-	"math"
-	"net/http"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-// TestStack is a parameterized set of test cases for stacks of various types:
-//   - strings
-//   - integers
-//	 - float64
-//	 - runes
-//	 - bool
-//   - structs
-//   - functions
-//	 - channels
-//   - stacks
-func TestStack(t *testing.T) {
+func TestStack_string(t *testing.T) {
+	stack := NewStack[string]()
+	stack.Push("A")
+	stack.Push("B")
+	assert.Equal(t, 2, stack.Len())
 
-	tests := []struct {
-		name  string
-		input []any
-		want  []any
-	}{
-		// Test stacks of strings
-		{"strings", []any{"Larry", "Curly", "Moe"}, []any{"Moe", "Curly", "Larry"}},
+	var item string
+	var err error
 
-		// Test stacks of integers
-		{"integers", []any{3, 1, 4, 2, 6}, []any{6, 2, 4, 1, 3}},
-
-		// Test stacks of float64
-		{"float64", []any{math.Pi, math.Phi, math.E, -17.3}, []any{-17.3, math.E, math.Phi, math.Pi}},
-
-		// Test stacks of runes
-		{"float64", []any{'a', 'b', 'c', '\u2023'}, []any{'\u2023', 'c', 'b', 'a'}},
-
-		// Test stacks of bool
-		{"bool", []any{true, false, false}, []any{false, false, true}},
-
-		// Test stacks of structs
-		{"tuples", []any{NewPoint(3, 4), NewPoint(-1, 17)}, []any{NewPoint(-1, 17), NewPoint(3, 4)}},
-
-		// Test stacks of maps
-		{"maps", []any{NewPointMap("5"), NewPointMap("3", "1", "2"), NewPointMap()}, []any{NewPointMap(), NewPointMap("3", "1", "2"), NewPointMap("5")}},
-
-		// Test stacks of functions
-		{"functions", []any{
-			http.NotFound,
-			xml.Escape,
-			flag.Bool,
-			math.Acos,
-		}, []any{
-			math.Acos,
-			flag.Bool,
-			xml.Escape,
-			http.NotFound,
-		},
-		},
-
-		// Test stacks of channels
-		{"channels", []any{new(chan int), new(chan bool)}, []any{new(chan bool), new(chan int)}},
-		
-		// Test stacks of stacks
-		{"stacks", []any{
-			NewStack[rune](),
-			NewStack[int](),
-			NewStack[string](),
-			NewStack[float64](),
-		}, []any{
-			NewStack[float64](),
-			NewStack[string](),
-			NewStack[int](),
-			NewStack[rune](),
-		},
-		},
+	if item, err = stack.Pop(); err != nil {
+		t.Error(err)
+	} else {
+		letter := "B"
+		assert.Equal(t, item, letter)
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			stack := NewStack[any]()
-			wants := make([]any, 0)
 
-			// Load the stack with the Push() method
-			n := 0
-			for i, item := range tt.input {
-				stack.Push(item)
-				wantItem := tt.want[i]
-				wants = append(wants, wantItem)
-				n++
-			}
-
-			// Test the Peek() method
-			if n > 0 {
-				want := wants[0]
-				wantType := fmt.Sprintf("%T", want)
-				have := stack.Peek()
-				if strings.HasPrefix(wantType, "func") {
-					// Cannot compare functions for equal
-				} else {
-					assert.Equal(t, want, have, fmt.Sprintf("Peek: want=%v,have=%v\n", want, have))
-				}
-			}
-
-			// Test the Pop() method
-			assert.Equal(t, n, stack.Len())
-			for i := 0; i < n; i++ {
-				want := wants[i]
-				wantType := fmt.Sprintf("%T", want)
-				have := stack.Pop()
-				if strings.HasPrefix(wantType, "func") {
-					// Cannot compare functions for equal
-				} else {
-					assert.Equal(t, want, have, fmt.Sprintf("Pop: want=%v,have=%v\n", want, have))
-				}
-			}
-
-			// Stack should be empty now
-			assert.True(t, stack.IsEmpty())
-
-			// Test the Peek() and Pop() methods on an empty stack
-			var empty any
-			assert.Equal(t, empty, stack.Peek())
-			assert.Equal(t, empty, stack.Pop())
-		})
+	if item, err = stack.Pop(); err != nil {
+		t.Error(err)
+	} else {
+		letter := "A"
+		assert.Equal(t, item, letter)
 	}
 }
 
-// For testing a stack of structures
-type Point struct {
-	row, col int
-}
-
-func NewPoint(r, c int) Point {
-	p := new(Point)
-	p.row = r
-	p.col = c
-	return *p
-}
-
-// For testing a stack of maps
-type PointMap map[Point]string
-
-func NewPointMap(s ...string) PointMap {
-	pm := make(map[Point]string)
-	for i := 0; i < len(s); i++ {
-		n := int(s[i][0])
-		point := NewPoint(n, n+1)
-		pm[point] = s[i]
+func TestStack_struct_contents_same(t *testing.T) {
+	type Point struct {
+		x, y int
 	}
-	return pm
+	stack := NewStack[Point]()
+	stack.Push(Point{3, 4})
+	if item, err := stack.Pop(); err != nil {
+		t.Error(err)
+	} else {
+		wantX := 3
+		wantY := 4
+		assert.Equal(t, item.x, wantX)
+		assert.Equal(t, item.y, wantY)
+	}
 }
 
+func TestStack_map_contents_same(t *testing.T) {
+
+	original := make(map[string]int)
+	original["Moe"] = 1
+	original["Larry"] = 2
+	original["Shemp"] = 3
+
+	noCurly := make(map[string]int)
+	noCurly["Larry"] = 2
+	noCurly["Moe"] = 1
+
+	classic := make(map[string]int)
+	classic["Larry"] = 2
+	classic["Curly"] = 3
+	classic["Moe"] = 1
+
+	stack := NewStack[map[string]int]()
+	stack.Push(original)
+	stack.Push(noCurly)
+	stack.Push(classic)
+
+	assert.Equal(t, 3, stack.Len())
+
+	var item map[string]int
+	var err error
+	
+	if item, err = stack.Pop(); err != nil {
+		t.Error(err)
+	} else {
+		// Classic
+		assert.Equal(t, 1, item["Moe"])
+		assert.Equal(t, 2, item["Larry"])
+		assert.Equal(t, 3, item["Curly"])
+	}
+	
+	if item, err = stack.Pop(); err != nil {
+		t.Error(err)
+	} else {
+		// NoCurly
+		assert.Equal(t, 1, item["Moe"])
+		assert.Equal(t, 2, item["Larry"])
+		_, ok := item["Curly"]
+		assert.False(t, ok)
+	}
+	
+	if item, err = stack.Pop(); err != nil {
+		t.Error(err)
+	} else {
+		// Original
+		assert.Equal(t, 1, item["Moe"])
+		assert.Equal(t, 2, item["Larry"])
+		assert.Equal(t, 3, item["Shemp"])
+	}
+}
